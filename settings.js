7bf92359d964911cc5787577461a0a859deef6d1
Nesianone <terryd+github@gmail.com>
2026-04-23 07:42:57 +1200

    fix: revoke object URL asynchronously in triggerDownload to prevent Firefox download failure

--- settings.js
+++ settings.js
@@ -329,5 +329,5 @@ function triggerDownload(blob, filename) {
   const url = URL.createObjectURL(blob);
   const a   = document.createElement('a');
   a.href = url; a.download = filename; a.click();
-  URL.revokeObjectURL(url);
+  setTimeout(() => URL.revokeObjectURL(url), 100);
 }
